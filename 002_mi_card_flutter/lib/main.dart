35f556ae69e77605a8d2fad9aad9504a74173052
Rainer Gevers <rainergeve@gmail.com>
2020-06-26 17:00:20 +0200

    played with some more components

--- 002_mi_card_flutter/lib/main.dart
+++ 002_mi_card_flutter/lib/main.dart
@@ -14,21 +14,22 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
               Container(
                 height: 100,
-                width: 100,
                 color: Colors.white,
                 child: Text('Container 1'),
               ),
+              SizedBox(
+                height: 20,
+              ),
               Container(
-                width: 100,
                 height: 100,
                 color: Colors.blue,
                 child: Text('Container 2'),
               ),
               Container(
-                width: 100,
                 height: 100,
                 color: Colors.red,
                 child: Text('Container 3'),
