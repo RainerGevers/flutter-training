bb30fa9575330e324512749d9664724e0b26957c
Rainer Gevers <rainer@fastcomm.com>
2020-07-03 09:07:10 +0200

    Updated clima

--- 008_Clima/lib/screens/loading_screen.dart
+++ 008_Clima/lib/screens/loading_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:clima/services/location.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 
 class LoadingScreen extends StatefulWidget {
@@ -10,8 +12,24 @@ class LoadingScreen extends StatefulWidget {
 class _LoadingScreenState extends State<LoadingScreen> {
 
   void getLocation() async {
-    Position position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
-    print(position);
+    Location location = Location();
+    await location.getCurrentLocation();
+    print(location.latitude);
+    print(location.longitude);
+  }
+
+  void getData() async {
+    http.Response response = await http.get('https://samples.openweathermap.org/data/2.5/weather?id=2172797&appid=439d4b804bc8187953eb36d2a8c26a02');
+    if (response.statusCode == 200) {
+      var jsonData = jsonDecode(response.body);
+      double temperature = jsonData['main']['temp'];
+      int condition = jsonData['weather'][0]['id'];
+      String cityName = jsonData['name'];
+      print('$temperature - $condition - $cityName');
+
+    } else {
+      print(response.statusCode);
+    }
   }
 
   @override
@@ -22,6 +40,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold(
       );
   }
